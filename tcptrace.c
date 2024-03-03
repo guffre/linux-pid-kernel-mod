7bd40af695f6547f62dd588c9731db22c1889deb
Michael Guffre <mtguffre@gmail.com>
2024-03-03 01:28:15 -0600

    Modifies sent/received packets

--- tcptrace.c
+++ tcptrace.c
@@ -6,75 +6,103 @@
 #include <linux/sched.h>
 #include <net/tcp.h>
 
+// Maximum number of probes
 #define MAX_PROBES 10
 
-static struct kretprobe probes[MAX_PROBES];
+static struct kretprobe probes[MAX_PROBES];	// Probe handles
+int inside_tcp = 0;	// Global to keep track of recv inside TCP or not
 
 struct args {
-	unsigned long arg0;
-	unsigned long arg1;
-	unsigned long arg2;
+	struct msghdr *msg;
+	int data_len;
+	struct iov_iter *msg_iter;
 };
+
+// static int secure_tcp_seq_ret_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
+// {
+// 	// struct args *data;
+// 	// data = (struct args *)ri->data;
+// 	unsigned int retval = regs_return_value(regs);
+// 	printk(KERN_INFO "%s returned %u\n", "secure_tcp_seq", retval);
+// 	return 0;
+// }
 
 static int tcp_connect_ent_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
 {
 	struct sock *sk = (void *)regs_get_kernel_argument(regs, 0);
 	struct tcp_sock *tp = tcp_sk(sk);
-	printk(KERN_INFO "%s write_seq: %u\n", "tcp_connect", tp->write_seq);
+	printk(KERN_INFO "%s: SYN: %u\n", "tcp_connect", tp->write_seq);
 	return 0;
 }
 
 static int tcp_v4_init_seq_ret_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
 {
 	unsigned int retval = regs_return_value(regs);
-	printk(KERN_INFO "%s returned %u\n", "tcp_v4_init_seq", retval);
-	return 0;
-}
-
-static int secure_tcp_seq_ret_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
-{
-	unsigned int retval = regs_return_value(regs);
-	printk(KERN_INFO "%s returned %u\n", "secure_tcp_seq", retval);
-	return 0;
-}
-
-
-static int tcp_v4_connect_ent_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
-{
-	struct args *data;
-	data = (struct args *)ri->data;
-	data->arg0 = regs_get_kernel_argument(regs, 0);
+	printk(KERN_INFO "%s: ACK: %u\n", "tcp_v4_init_seq", retval);
 	return 0;
 }
 
 static int tcp_sendmsg_ent_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
 {
-	printk(KERN_INFO "%s entered.\n", "tcp_sendmsg");
-	char buffer[512];
 	struct iov_iter msg_iter_backup;
 	struct msghdr *msg = (void*)regs_get_kernel_argument(regs, 1);
 	size_t size = regs_get_kernel_argument(regs, 2);
 
-	if (size < 512)
+	if (msg->msg_iter.iter_type == 0) // USERBUF
 	{
-		msg_iter_backup = msg->msg_iter;
-		_copy_from_iter(buffer, size, &(msg->msg_iter));
-		msg->msg_iter = msg_iter_backup;
-		buffer[size] = '\0';
-		printk(KERN_INFO "%s [%lu]: %s\n", "tcp_sendmsg", size, buffer);
+		char* membuf = msg->msg_iter.__ubuf_iovec.iov_base;
+		for (int i = 0; i < size; i++)
+			membuf[i] ^= 'Z';
+		printk(KERN_INFO "membuf: %s\n", membuf);
 	}
+
 	return 0;
 }
 
-static int tcp_v4_connect_ret_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
+static int tcp_recvmsg_ent_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
 {
+	inside_tcp = 1;
+	printk(KERN_INFO "tcp_recvmsg: lock\n");
+	return 0;
+}
+
+static int tcp_recvmsg_ret_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
+{
+	inside_tcp = 0;
+	printk(KERN_INFO "tcp_recvmsg: unlock\n");
+	return 0;
+}
+
+static int skb_copy_datagram_iter_ent_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
+{
+	if (inside_tcp == 0)
+		return 0;
+
+	int size, offset;
+	unsigned long tmp;
+	struct sk_buff* skb;
+	struct iov_iter *msg_iter;
 	struct args *data;
+
 	data = (struct args *)ri->data;
 
-	struct sock *sk = (void*)data->arg0;
-	struct tcp_sock *tp = tcp_sk(sk);
+	tmp		= regs_get_kernel_argument(regs, 0);
+	if (tmp != 0) { skb = (struct sk_buff*)tmp; }
+	offset	= regs_get_kernel_argument(regs, 1);
+	tmp 	= regs_get_kernel_argument(regs, 2);
+	if (tmp != 0) { msg_iter = (struct iov_iter*)tmp; }
+	size	= regs_get_kernel_argument(regs, 3);
 
-	printk(KERN_INFO "%s write_seq: %u\n", "tcp_v4_connect", tp->write_seq);
+	data->data_len = size;
+	data->msg_iter = msg_iter;
+
+	char *buffer = kmalloc(size, GFP_ATOMIC);
+	skb_copy_bits(skb, offset, buffer, size);
+	for (int i = 0; i < size; i++)
+		buffer[i] ^= 'Z';
+	skb_store_bits(skb, offset, buffer, size);
+	printk("skb_copy_datagram_iter: [%d] %s\n", size, buffer);
+
 	return 0;
 }
 
@@ -100,11 +128,12 @@ static int __init kretprobe_init(void)
 	int i;
 	for (i = 0; i < MAX_PROBES; i++) { probes[i].data_size = 0; }
 	i = 0;
-	register_probe(&probes[i++], "tcp_v4_init_seq", &tcp_v4_init_seq_ret_handler, NULL);
-	register_probe(&probes[i++], "secure_tcp_seq", &secure_tcp_seq_ret_handler, NULL);
-	register_probe(&probes[i++], "tcp_v4_connect", &tcp_v4_connect_ret_handler, &tcp_v4_connect_ent_handler);
-	register_probe(&probes[i++], "tcp_connect", NULL, &tcp_connect_ent_handler);
-	register_probe(&probes[i++], "tcp_sendmsg_locked", NULL, &tcp_sendmsg_ent_handler);
+	//register_probe(&probes[i++], "secure_tcp_seq", &secure_tcp_seq_ret_handler, NULL);		// Generates SEQ/ACK numbers
+	register_probe(&probes[i++], "tcp_connect", NULL, &tcp_connect_ent_handler);			// SYN
+	register_probe(&probes[i++], "tcp_v4_init_seq", &tcp_v4_init_seq_ret_handler, NULL);	// SYN-ACK
+	register_probe(&probes[i++], "tcp_sendmsg", NULL, &tcp_sendmsg_ent_handler);			// Sent data
+	register_probe(&probes[i++], "tcp_recvmsg", &tcp_recvmsg_ret_handler, &tcp_recvmsg_ent_handler); 	// Received data boundary
+	register_probe(&probes[i++], "skb_copy_datagram_iter", NULL, &skb_copy_datagram_iter_ent_handler);  // Received data
 	return 0;
 }
 
