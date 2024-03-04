4d4fc35003f34d03a4709ec72ee26de0206a6123
Michael Guffre <mtguffre@gmail.com>
2024-03-04 09:56:08 -0600

    Start adding options to modify headers

--- tcptrace.c
+++ tcptrace.c
@@ -51,14 +51,25 @@ static int secure_tcp_seq_ret_handler(struct kretprobe_instance *ri, struct pt_r
 	data = (struct args *)ri->data;
 	unsigned long retval = regs_return_value(regs);
 	
-	disable_kretprobe(&(probes[0]));
+	// disable_kretprobe(&(probes[0])); // Buggy on some kernels?
 	while (!is_prime_number(retval))
 	{
+		// This should be replaced. Just generate a suitable prime number
 		retval = secure_tcp_seq(data->saddr, data->daddr, data->sport, data->dport);
 	}
+	// enable_kretprobe(&(probes[0])); // Buggy on some kernels?
 	regs_set_return_value(regs, retval);
 	printk(KERN_INFO "%s returned %u\n", "secure_tcp_seq", (u32)retval);
-	enable_kretprobe(&(probes[0]));
+	return 0;
+}
+
+// static void tcp_options_write(struct tcphdr *th, struct tcp_sock *tp,
+// 			      struct tcp_out_options *opts)
+static int tcp_options_write_ret_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
+{
+	struct tcphdr *th = (struct tcphdr *)regs_get_kernel_argument(regs, 0);
+	th->res1 |= 4; // Should set a reserved bit
+	printk(KERN_INFO "%s: Modified TCP Header\n", "tcp_options_write");
 	return 0;
 }
 
@@ -66,14 +77,14 @@ static int tcp_connect_ent_handler(struct kretprobe_instance *ri, struct pt_regs
 {
 	struct sock *sk = (void *)regs_get_kernel_argument(regs, 0);
 	struct tcp_sock *tp = tcp_sk(sk);
-	printk(KERN_INFO "%s: SYN: %u\n", "tcp_connect", tp->write_seq);
+	printk(KERN_INFO "%s: SYN (sent): %u\n", "tcp_connect", tp->write_seq);
 	return 0;
 }
 
 static int tcp_v4_init_seq_ret_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
 {
 	unsigned int retval = regs_return_value(regs);
-	printk(KERN_INFO "%s: ACK: %u\n", "tcp_v4_init_seq", retval);
+	printk(KERN_INFO "%s: ACK (sent): %u\n", "tcp_v4_init_seq", retval);
 	return 0;
 }
 
@@ -84,6 +95,7 @@ static int tcp_sendmsg_ent_handler(struct kretprobe_instance *ri, struct pt_regs
 
 	if (msg->msg_iter.iter_type == 0) // USERBUF
 	{
+		// Need to check if this is valid memory
 		char* membuf = msg->msg_iter.__ubuf_iovec.iov_base;
 		for (int i = 0; i < size; i++)
 			membuf[i] ^= 'Z';
@@ -95,6 +107,7 @@ static int tcp_sendmsg_ent_handler(struct kretprobe_instance *ri, struct pt_regs
 
 static int tcp_recvmsg_ent_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
 {
+	// Should be a semaphore or a real kernel lock
 	inside_tcp = 1;
 	printk(KERN_INFO "tcp_recvmsg: lock\n");
 	return 0;
@@ -102,6 +115,7 @@ static int tcp_recvmsg_ent_handler(struct kretprobe_instance *ri, struct pt_regs
 
 static int tcp_recvmsg_ret_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
 {
+	// Should be a semaphore or a real kernel lock
 	inside_tcp = 0;
 	printk(KERN_INFO "tcp_recvmsg: unlock\n");
 	return 0;
@@ -109,6 +123,7 @@ static int tcp_recvmsg_ret_handler(struct kretprobe_instance *ri, struct pt_regs
 
 static int skb_copy_datagram_iter_ent_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
 {
+	// Should be a semaphore or a real kernel lock
 	if (inside_tcp == 0)
 		return 0;
 
@@ -116,9 +131,9 @@ static int skb_copy_datagram_iter_ent_handler(struct kretprobe_instance *ri, str
 	unsigned long tmp;
 	struct sk_buff* skb;
 	struct iov_iter *msg_iter;
-	struct args *data;
+	// struct args *data;
 
-	data = (struct args *)ri->data;
+	// data = (struct args *)ri->data;
 
 	tmp		= regs_get_kernel_argument(regs, 0);
 	if (tmp != 0) { skb = (struct sk_buff*)tmp; }
@@ -127,9 +142,10 @@ static int skb_copy_datagram_iter_ent_handler(struct kretprobe_instance *ri, str
 	if (tmp != 0) { msg_iter = (struct iov_iter*)tmp; }
 	size	= regs_get_kernel_argument(regs, 3);
 
-	data->data_len = size;
-	data->msg_iter = msg_iter;
+	// data->data_len = size;
+	// data->msg_iter = msg_iter;
 
+	// This is a temporary check for testing. Need to mark in the TCP header to decrypt/encrypt
 	if (msg_iter->iter_type == 0) // USER_BUF
 	{
 		char *buffer = kmalloc(size, GFP_ATOMIC);
@@ -157,6 +173,7 @@ void register_probe(struct kretprobe *probe, char *func_name, int(*ret_handler)(
 	ret = register_kretprobe(probe);
 	if (ret < 0) {
 		printk(KERN_INFO "register_kretprobe failed, returned %d\n", ret);
+		(*probe).data_size = 0;
 	}
 	printk(KERN_INFO "Planted return probe at %s: %p\n", (*probe).kp.symbol_name, (*probe).kp.addr);
 }
@@ -178,13 +195,16 @@ static int __init kretprobe_init(void)
 static void __exit kretprobe_exit(void)
 {
 	int i;
-	for (i = 0; probes[i].data_size != 0; i++)
+	for (i = 0; i < MAX_PROBES; i++)
 	{
-		unregister_kretprobe(&(probes[i]));
-		printk(KERN_INFO "kretprobe at %p unregistered\n", probes[i].kp.addr);
-
-		/* nmissed > 0 suggests that maxactive was set too low. */
-		printk(KERN_INFO "Missed probing %d instances of %s\n", probes[i].nmissed, probes[i].kp.symbol_name);
+		if (probes[i].data_size != 0)
+		{
+			unregister_kretprobe(&(probes[i]));
+			printk(KERN_INFO "kretprobe at %p unregistered\n", probes[i].kp.addr);
+	
+			/* nmissed > 0 suggests that maxactive was set too low. */
+			printk(KERN_INFO "Missed probing %d instances of %s\n", probes[i].nmissed, probes[i].kp.symbol_name);
+		}
 	}
 }
 
